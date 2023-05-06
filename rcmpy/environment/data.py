920b57e183f6695285fcfeeb107bc367994b74e6
Vaughn Kottler <vaughnkottler@gmail.com>
2023-05-06 18:27:55 -0500

    Small updates

--- rcmpy/environment/data.py
+++ rcmpy/environment/data.py
@@ -3,17 +3,18 @@ A module for exposing useful information from the current system to templates.
 """
 
 # built-in
+from functools import lru_cache
 import platform
 import sys
 from typing import Any, Dict
 
 
+@lru_cache(maxsize=1)
 def system_data() -> Dict[str, Any]:
     """Obtain useful system configuration data."""
 
     return {
         "sys": {
-            "abiflags": sys.abiflags,
             "base_exec_prefix": sys.base_exec_prefix,
             "base_prefix": sys.base_prefix,
             "byteorder": sys.byteorder,
